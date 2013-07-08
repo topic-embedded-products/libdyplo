a74e4c610c813e190baa9a745196c96c35ce00f6
Mike Looijmans <mike.looijmans@topic.nl>
2013-07-08 08:18:32 +0200

    Add extra "borderline" test cases

--- testhmu.cpp
+++ testhmu.cpp
@@ -121,18 +121,45 @@ FUNC(a_fixed_memory_queue_without_scheduler)
 	for (unsigned int b = 0; b < capacity; ++b)
 	{
 		int* data;
-		q.begin_write(data, 1);
+		YAFFUT_EQUAL(capacity - b, q.begin_write(data, 1));
 		q.end_write(1);
 	}
 	YAFFUT_CHECK(q.full());
 	for (unsigned int b = 0; b < capacity; ++b)
 	{
 		int* data;
-		q.begin_read(data, 1);
+		YAFFUT_EQUAL(capacity - b, q.begin_read(data, 1));
 		q.end_read(1);
 	}
 	YAFFUT_CHECK(q.empty());
 }
+
+FUNC(a_fixed_memory_queue_border_cases)
+{
+	hmu::FixedMemoryQueue<int, hmu::NoopScheduler> q(5);
+	int* data;
+
+	YAFFUT_EQUAL(5, q.begin_write(data, 1));
+	data[0] = 1;
+	data[1] = 2;
+	data[2] = 3;
+	q.end_write(3);
+	YAFFUT_EQUAL(3, q.begin_read(data, 1));
+	YAFFUT_EQUAL(2, data[1]);
+	q.end_read(2);
+	YAFFUT_EQUAL(2, q.begin_write(data, 1)); /* Leave 1 item */
+	data[0] = 4;
+	data[1] = 5;
+	q.end_write(1);
+	q.end_write(1);
+	YAFFUT_EQUAL(2, q.begin_write(data, 1));
+	YAFFUT_EQUAL(3, q.begin_read(data, 1));
+	q.end_write(1);
+	YAFFUT_EQUAL(1, q.begin_write(data, 1));
+	q.end_read(2);
+	YAFFUT_EQUAL(3, q.begin_write(data, 1));
+}
+
 
 FUNC(fixed_memory_queue_cooperative_scheduler)
 {
