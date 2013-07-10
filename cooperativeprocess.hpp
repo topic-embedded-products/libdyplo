93dc9e66c647b61c6bc1766e87297cc1955a62be
Mike Looijmans <mike.looijmans@topic.nl>
2013-07-10 11:03:51 +0200

    Make the cooperativeprocess implementation overridable
    Allow the user to implement the actual function of the process. The
    test case uses a simple "add 1 to input" example.

--- cooperativeprocess.hpp
+++ cooperativeprocess.hpp
@@ -8,15 +8,23 @@ namespace dyplo
 	template <class InputQueueClass, class OutputQueueClass, int blocksize = 1> class CooperativeProcess: public Process
 	{
 	public:
-		InputQueueClass &input;
-		OutputQueueClass &output;
+		InputQueueClass *input;
+		OutputQueueClass *output;
 
-		CooperativeProcess(InputQueueClass &input_, OutputQueueClass &output_):
-			input(input_),
-			output(output_)
+		void set_input(InputQueueClass *value)
 		{
-			input.get_scheduler().downstream = this;
+			input = value;
+			input->get_scheduler().downstream = this;
 		}
+
+		void set_output(OutputQueueClass *value)
+		{
+			output = value;
+		}
+
+		virtual void process_block(
+			typename OutputQueueClass::Element *dest,
+			typename InputQueueClass::Element *src) = 0;
 
 		virtual void process_one()
 		{
@@ -26,16 +34,13 @@ namespace dyplo
 
 			for (;;)
 			{
-				count = input.begin_read(src, 0);
+				count = input->begin_read(src, 0);
 				if (count < blocksize)
 					return;
-				output.begin_write(dest, blocksize);
-				for (int i = 0; i < blocksize; ++i)
-				{
-					*dest++ = (*src++) + 1;
-				}
-				output.end_write(blocksize);
-				input.end_read(blocksize);
+				output->begin_write(dest, blocksize);
+				process_block(dest, src);
+				output->end_write(blocksize);
+				input->end_read(blocksize);
 			}
 		}
 	};
--- testdyplocooperative.cpp
+++ testdyplocooperative.cpp
@@ -5,24 +5,36 @@
 
 #include "yaffut.h"
 
+template <class T, class OutputQueue, int blocksize = 1> class AddOne: public dyplo::CooperativeProcess<
+		dyplo::FixedMemoryQueue<T, dyplo::CooperativeScheduler>,
+		OutputQueue,
+		blocksize>
+{
+	void process_block(T* dest, T* src)
+	{
+		for (int i = 0; i < blocksize; ++i)
+			*dest++ = (*src++) + 1;
+	}
+};
+
+
 FUNC(fixed_memory_queue_cooperative_scheduler)
 {
 	dyplo::FixedMemoryQueue<int, dyplo::CooperativeScheduler> input(1);
 	dyplo::FixedMemoryQueue<int, dyplo::NoopScheduler> output(2);
 
-	dyplo::CooperativeProcess<
-		dyplo::FixedMemoryQueue<int, dyplo::CooperativeScheduler>,
-		dyplo::FixedMemoryQueue<int, dyplo::NoopScheduler> >
-			proc(input, output);
+	AddOne<int, dyplo::FixedMemoryQueue<int, dyplo::NoopScheduler>, 1> proc;
+	proc.set_input(&input);
+	proc.set_output(&output);
 
-	YAFFUT_CHECK(proc.input.empty());
-	YAFFUT_CHECK(proc.output.empty());
+	YAFFUT_CHECK(input.empty());
+	YAFFUT_CHECK(output.empty());
 
-	proc.input.push_one(42);
-	YAFFUT_EQUAL(1, proc.output.size());
-	YAFFUT_CHECK(proc.input.empty());
-	YAFFUT_EQUAL(43, proc.output.pop_one());
-	YAFFUT_CHECK(proc.output.empty());
+	input.push_one(42);
+	YAFFUT_EQUAL(1, output.size());
+	YAFFUT_CHECK(input.empty());
+	YAFFUT_EQUAL(43, output.pop_one());
+	YAFFUT_CHECK(output.empty());
 }
 
 FUNC(fixed_memory_queue_cooperative_scheduler_multi)
@@ -30,24 +42,22 @@ FUNC(fixed_memory_queue_cooperative_scheduler_multi)
 	dyplo::FixedMemoryQueue<int, dyplo::CooperativeScheduler> input(2);
 	dyplo::FixedMemoryQueue<int, dyplo::NoopScheduler> output(2);
 
-	dyplo::CooperativeProcess<
-		dyplo::FixedMemoryQueue<int, dyplo::CooperativeScheduler>,
-		dyplo::FixedMemoryQueue<int, dyplo::NoopScheduler>,
-		2>
-			proc(input, output);
+	AddOne<int, dyplo::FixedMemoryQueue<int, dyplo::NoopScheduler>, 2> proc;
+	proc.set_input(&input);
+	proc.set_output(&output);
 
-	YAFFUT_CHECK(proc.input.empty());
-	YAFFUT_CHECK(proc.output.empty());
+	YAFFUT_CHECK(input.empty());
+	YAFFUT_CHECK(output.empty());
 
-	proc.input.push_one(42);
-	YAFFUT_CHECK(proc.output.empty());
-	proc.input.push_one(43);
-	YAFFUT_EQUAL(2, proc.output.size());
-	YAFFUT_CHECK(proc.input.empty());
+	input.push_one(42);
+	YAFFUT_CHECK(output.empty());
+	input.push_one(43);
+	YAFFUT_EQUAL(2, output.size());
+	YAFFUT_CHECK(input.empty());
 
-	YAFFUT_EQUAL(43, proc.output.pop_one());
-	YAFFUT_EQUAL(44, proc.output.pop_one());
-	YAFFUT_CHECK(proc.output.empty());
+	YAFFUT_EQUAL(43, output.pop_one());
+	YAFFUT_EQUAL(44, output.pop_one());
+	YAFFUT_CHECK(output.empty());
 }
 
 FUNC(multiple_processes_and_queues)
@@ -55,16 +65,30 @@ FUNC(multiple_processes_and_queues)
 	dyplo::FixedMemoryQueue<int, dyplo::CooperativeScheduler> input_to_a(1);
 	dyplo::FixedMemoryQueue<int, dyplo::CooperativeScheduler> between_a_and_b(1);
 	dyplo::FixedMemoryQueue<int, dyplo::NoopScheduler> output_from_b(2);
-	
-	dyplo::CooperativeProcess <
-		dyplo::FixedMemoryQueue<int, dyplo::CooperativeScheduler>,
-		dyplo::FixedMemoryQueue<int, dyplo::CooperativeScheduler> >
-			a(input_to_a, between_a_and_b);
-	dyplo::CooperativeProcess<
-		dyplo::FixedMemoryQueue<int, dyplo::CooperativeScheduler>,
-		dyplo::FixedMemoryQueue<int, dyplo::NoopScheduler> >
-			b(between_a_and_b, output_from_b);
+
+	AddOne<int, dyplo::FixedMemoryQueue<int, dyplo::CooperativeScheduler> > a;
+	AddOne<int, dyplo::FixedMemoryQueue<int, dyplo::NoopScheduler> > b;
+
+	a.set_input(&input_to_a);
+	a.set_output(&between_a_and_b);
+	b.set_input(&between_a_and_b);
+	b.set_output(&output_from_b);
 
 	input_to_a.push_one(50);
 	YAFFUT_EQUAL(52, output_from_b.pop_one());
+	input_to_a.push_one(51);
+	YAFFUT_EQUAL(53, output_from_b.pop_one());
+	input_to_a.push_one(52);
+	YAFFUT_EQUAL(54, output_from_b.pop_one());
+
+	YAFFUT_EQUAL(0, input_to_a.size());
+	YAFFUT_EQUAL(0, between_a_and_b.size());
+	YAFFUT_EQUAL(0, output_from_b.size());
+
+	input_to_a.push_one(60);
+	YAFFUT_EQUAL(0, input_to_a.size());
+	YAFFUT_EQUAL(0, between_a_and_b.size());
+	YAFFUT_EQUAL(1, output_from_b.size());
+	input_to_a.push_one(61);
+	YAFFUT_EQUAL(2, output_from_b.size());
 }
