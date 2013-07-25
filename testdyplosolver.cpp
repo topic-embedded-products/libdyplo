4a36c0c5218c912a2898e4545ed39ce506da4260
Mike Looijmans <mike.looijmans@topic.nl>
2013-07-25 14:42:37 +0200

    solver: Add a weighted optimizer (in test code)
    Given a set of functions and bitmasks on which partitions they can be
    implementeds, and a score for each function, calculate the distribution
    that yields the optimal score.
    
    Reduce the search space by calculating the maximum score that can be
    attained for a certain node. If the current score is equal to that,
    stop searching for anything better.

--- testdyplosolver.cpp
+++ testdyplosolver.cpp
@@ -6,16 +6,23 @@
 class Partitioner
 {
 public:
-	std::vector<int> function_masks;
-	std::vector<int> solution;
+	std::vector<unsigned int> function_masks;
+	std::vector<unsigned int> solution;
 	
+	int solve(unsigned int available_mask)
+	{
+		solution.resize(function_masks.size());
+		return recurse(0, available_mask);
+	}
+
+protected:
 	int recurse(int function_index, unsigned int mask)
 	{
 		if (function_index >= function_masks.size())
 			return 0;
 		for (unsigned int index = 0, index_bit = 1;
-			 mask >= index_bit /*index < number_of_partitions*/ ;
-			++index, index_bit <<= 1)
+			 mask >= index_bit;
+			 ++index, index_bit <<= 1)
 		{
 			if (mask & index_bit)
 			{
@@ -32,14 +39,90 @@ public:
 		}
 		return -1;
 	}
-	
-	int solve(unsigned int available_mask)
-	{
-		solution.resize(function_masks.size());
-		return recurse(0, available_mask);
-	}
 };
 
+class WeightedPartitioner
+{
+public:
+	std::vector<unsigned int> function_masks;
+	std::vector<unsigned int> function_scores;
+	std::vector<int> solution;
+	
+	unsigned int solve(unsigned int available_mask)
+	{
+		solution.assign(function_masks.size(), -1);
+		best_score.assign(function_masks.size(), 0);
+		return recurse(0, available_mask, 0);
+	}
+	
+	unsigned int calc_max_attainable_score(int function_index, unsigned int mask)
+	{
+		unsigned int result = 0;
+		while (function_index < function_masks.size())
+		{
+			if (function_masks[function_index] & mask)
+				result += function_scores[function_index];
+			++function_index;
+		}
+		return result;
+	}
+	
+	void check_valid()
+	{
+		const int num_functions = solution.size();
+		EQUAL(num_functions, function_masks.size());
+		for (int i = 0; i < num_functions-1; ++i)
+			for (int j=i+1; j < num_functions; ++j)
+				CHECK(solution[i] != solution[j]);
+	}
+
+protected:
+	std::vector<unsigned int> best_score;
+
+	unsigned int recurse(int function_index, unsigned int mask, unsigned int score)
+	{
+		if (function_index >= function_masks.size())
+			return score;
+		const unsigned int max_attainable_score = calc_max_attainable_score(function_index, mask);
+		unsigned int max_score = score;
+		int local_solution = -1;
+		for (unsigned int index = 0, index_bit = 1;
+			 (mask >= index_bit) && index_bit && ((max_score - score) < max_attainable_score);
+			 ++index, index_bit <<= 1)
+		{
+			if (mask & index_bit)
+			{
+				if (function_masks[function_index] & index_bit)
+				{
+					unsigned int r = recurse(function_index+1, mask & ~index_bit, score + function_scores[function_index]);
+					if (r > max_score)
+					{
+						local_solution = index;
+						max_score = r;
+					}
+				}
+			}
+		}
+		/* What if we do NOT pick this function */
+		if ((max_score - score) < max_attainable_score)
+		{
+			unsigned int r = recurse(function_index+1, mask, score);
+			if (r > max_score)
+			{
+				local_solution = -1;
+				max_score = r;
+			}
+		}
+		
+		if (max_score > best_score[function_index])
+		{
+			best_score[function_index] = max_score;
+			solution[function_index] = local_solution;
+		}
+	
+		return max_score;
+	}
+};
 
 FUNC(solve_partition_configuration)
 {
@@ -92,7 +175,7 @@ FUNC(solve_partition_configuration_with_mask)
 	p.function_masks.push_back(0b110011);
 	p.function_masks.push_back(0b110110);
 	/* No solution if partition 0 is in use */
-	YAFFUT_CHECK(p.solve(0xb1110) < 0);
+	YAFFUT_CHECK(p.solve(0b1110) < 0);
 
 	p.function_masks[0] = 0b1101;
 	p.function_masks[1] = 0b0111;
@@ -105,4 +188,63 @@ FUNC(solve_partition_configuration_with_mask)
 	EQUAL(0, p.solution[0]);
 	EQUAL(1, p.solution[1]);
 	EQUAL(3, p.solution[2]);
+}
+
+FUNC(solve_partition_configuration_with_weight)
+{
+	WeightedPartitioner p;
+	p.function_masks.push_back(0b001);
+	p.function_masks.push_back(0b010);
+	p.function_masks.push_back(0b100);
+	p.function_scores.push_back(1);
+	p.function_scores.push_back(2);
+	p.function_scores.push_back(3);
+	
+	EQUAL(3, p.calc_max_attainable_score(0, 0b011));
+	EQUAL(5, p.calc_max_attainable_score(0, 0b110));
+	EQUAL(5, p.calc_max_attainable_score(1, 0b110));
+	EQUAL(2, p.calc_max_attainable_score(1, 0b011));
+	
+	unsigned int r = p.solve(0b111);
+	EQUAL(6U, p.calc_max_attainable_score(0, 0b111));
+	EQUAL(5U, p.calc_max_attainable_score(1, 0b111));
+	EQUAL(3U, p.calc_max_attainable_score(2, 0b111));
+	EQUAL(6, r);
+	EQUAL(0, p.solution[0]);
+	EQUAL(1, p.solution[1]);
+	EQUAL(2, p.solution[2]);
+	p.check_valid();
+	p.function_masks[0] = 0b001;
+	p.function_masks[1] = 0b010;
+	p.function_masks[2] = 0b010;
+	EQUAL(4, p.solve(0b111));
+	EQUAL(0, p.solution[0]);
+	EQUAL(-1, p.solution[1]);
+	EQUAL(1, p.solution[2]);
+	p.check_valid();
+
+	p.function_masks[0] = 0b011;
+	p.function_masks[1] = 0b011;
+	p.function_masks[2] = 0b011;
+	EQUAL(5, p.solve(0b011));
+	EQUAL(-1, p.solution[0]);
+	EQUAL(0, p.solution[1]);
+	EQUAL(1, p.solution[2]);
+	p.check_valid();
+	
+	EQUAL(6, p.calc_max_attainable_score(0, 0b011));
+
+	/* Create a large search space. The actual test here is that this
+	 * finished in reasonable time. With the initial implementation without
+	 * score estimations, the runtime on an i7 is several hours... */
+	const unsigned int functions = 8;
+	const unsigned int partitions = 30;
+	p.function_masks.assign(functions, (1 << partitions) - 1);
+	p.function_scores.assign(functions, 1);
+	EQUAL(std::min(functions, partitions), p.solve((1 << partitions) - 1));
+	p.check_valid();
+	
+	p.function_masks[functions-1] = 0;
+	EQUAL(std::min(functions-1, partitions), p.solve((1 << partitions) - 1));
+	p.check_valid();
 }
