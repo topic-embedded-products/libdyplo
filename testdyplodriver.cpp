2221865b0817759747b1a8fdaa4128c6a8d6588e
Mike Looijmans <mike.looijmans@topic.nl>
2013-08-14 09:17:42 +0200

    Add driver tests for read/write access

--- testdyplodriver.cpp
+++ testdyplodriver.cpp
@@ -32,14 +32,27 @@ FUNC(hardware_driver_control_multiple_open)
 
 FUNC(hardware_driver_config_single_open)
 {
-	File cfg0("/dev/dyplocfg0");
-	File cfg1("/dev/dyplocfg1"); /* Other cfg can be opened */
-	ASSERT_THROW(File another_cfg0("/dev/dyplocfg0"), dyplo::IOException);
+	File cfg0r("/dev/dyplocfg0");
+	File cfg1r("/dev/dyplocfg1"); /* Other cfg can be opened */
+	ASSERT_THROW(File another_cfg0r("/dev/dyplocfg0"), dyplo::IOException);
+	/* Can open same file with other access rights */
+	File cfg0w("/dev/dyplocfg0", O_WRONLY);
+	/* But cannot open it twice either */
+	ASSERT_THROW(File another_cfg0w("/dev/dyplocfg0", O_WRONLY), dyplo::IOException);
+	/* Open for both R/W */
+	File cfg3rw("/dev/dyplocfg3", O_RDWR);
+	ASSERT_THROW(File another_cfg3rw("/dev/dyplocfg3", O_RDONLY), dyplo::IOException);
+	ASSERT_THROW(File another_cfg3rw("/dev/dyplocfg3", O_WRONLY), dyplo::IOException);
 }
 
-FUNC(hardware_driver_fifo_single_open)
+FUNC(hardware_driver_fifo_single_open_rw_access)
 {
 	File r0("/dev/dyplor0");
 	File r1("/dev/dyplor1"); /* Other fifo can be opened */
 	ASSERT_THROW(File another_r0("/dev/dyplor0"), dyplo::IOException);
+	ASSERT_THROW(File another_r1("/dev/dyplor1"), dyplo::IOException);
+	/* Cannot open input fifo for writing */
+	ASSERT_THROW(File r2w("/dev/dyplor2", O_WRONLY), dyplo::IOException);
+	/* Cannot open output fifo for reading */
+	ASSERT_THROW(File w0r("/dev/dyplow0", O_RDONLY), dyplo::IOException);
 }
