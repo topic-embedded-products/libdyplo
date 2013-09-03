7c8bed33229c97b0b2a32f5c19b71b40021a8cea
Mike Looijmans <mike.looijmans@topic.nl>
2013-09-03 07:55:02 +0200

    testdyplodriver: Factor out the hardcoded "/dev/" references
    Use HardwareContext instead.

--- testdyplodriver.cpp
+++ testdyplodriver.cpp
@@ -35,25 +35,27 @@ FUNC(hardware_driver_a_control_multiple_open)
 
 FUNC(hardware_driver_b_config_single_open)
 {
-	File cfg0r("/dev/dyplocfg0");
-	File cfg1r("/dev/dyplocfg1"); /* Other cfg can be opened */
-	ASSERT_THROW(File another_cfg0r("/dev/dyplocfg0"), dyplo::IOException);
+	dyplo::HardwareContext ctrl;
+	File cfg0r(ctrl.openConfig(0, O_RDONLY));
+	File cfg1r(ctrl.openConfig(1, O_RDONLY)); /* Other cfg can be opened */
+	ASSERT_THROW(File another_cfg0r(ctrl.openConfig(0, O_RDONLY)), dyplo::IOException);
 	/* Can open same file with other access rights */
-	File cfg0w("/dev/dyplocfg0", O_WRONLY);
+	File cfg0w(ctrl.openConfig(0, O_WRONLY));
 	/* But cannot open it twice either */
-	ASSERT_THROW(File another_cfg0w("/dev/dyplocfg0", O_WRONLY), dyplo::IOException);
+	ASSERT_THROW(File another_cfg0w(ctrl.openConfig(0, O_WRONLY)), dyplo::IOException);
 	/* Open for both R/W */
-	File cfg3rw("/dev/dyplocfg2", O_RDWR);
-	ASSERT_THROW(File another_cfg3rw("/dev/dyplocfg2", O_RDONLY), dyplo::IOException);
-	ASSERT_THROW(File another_cfg3rw("/dev/dyplocfg2", O_WRONLY), dyplo::IOException);
+	File cfg3rw(ctrl.openConfig(2, O_RDWR));
+	ASSERT_THROW(File another_cfg3rw(ctrl.openConfig(2, O_RDONLY)), dyplo::IOException);
+	ASSERT_THROW(File another_cfg3rw(ctrl.openConfig(2, O_WRONLY)), dyplo::IOException);
 }
 
 FUNC(hardware_driver_c_fifo_single_open_rw_access)
 {
-	File r0("/dev/dyplor0");
-	File r1("/dev/dyplor1"); /* Other fifo can be opened */
-	ASSERT_THROW(File another_r0("/dev/dyplor0"), dyplo::IOException);
-	ASSERT_THROW(File another_r1("/dev/dyplor1"), dyplo::IOException);
+	dyplo::HardwareContext ctrl;
+	File r0(ctrl.openFifo(0, O_RDONLY));
+	File r1(ctrl.openFifo(1, O_WRONLY)); /* Other fifo can be opened */
+	ASSERT_THROW(File another_r0(ctrl.openFifo(0, O_RDONLY)), dyplo::IOException);
+	ASSERT_THROW(File another_r1(ctrl.openFifo(1, O_WRONLY)), dyplo::IOException);
 	/* Cannot open input fifo for writing */
 	ASSERT_THROW(File r2w("/dev/dyplor2", O_WRONLY), dyplo::IOException);
 	/* Cannot open output fifo for reading */
