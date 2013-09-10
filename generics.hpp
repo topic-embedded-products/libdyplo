753ab7553d11de36e9ecf71c18f40301b3ddedc2
Mike Looijmans <mike.looijmans@topic.nl>
2013-09-10 15:19:20 +0200

    generics.hpp: Only compile in DEBUG_ASSERT conditionally
    Intended to be used in testing only, so disable by default and
    enable only when _DEBUG is defined.

--- generics.hpp
+++ generics.hpp
@@ -1,4 +1,8 @@
 #pragma once
 
-#define DEBUG_ASSERT(x, e) if (!(x)) throw std::runtime_error(e);
+#ifdef _DEBUG
+#	define DEBUG_ASSERT(x, e) if (!(x)) throw std::runtime_error(e)
+#else
+#	define DEBUG_ASSERT(x, e) while (false) {}
+#endif
 
