801659ee9b92aea0a5f178235c0f9b10dab9b624
Mike Looijmans <mike.looijmans@topic.nl>
2013-10-10 09:33:01 +0200

    thread: Reduce stack size to 128k
    Dyplo may use many threads, so reduce the virtual memory load by
    setting a much smaller stack size for threads created by dyplo.
    Default stack size on most systems is 8MB.

--- thread.hpp
+++ thread.hpp
@@ -9,6 +9,10 @@ namespace dyplo
 	protected:
 		pthread_t m_thread;
 	public:
+		/* Use a small thread size for Dyplo-managed threads. The system
+		 * default is usually 8MB, resulting in a high load on VM */
+		static const int default_stack_size = 128 * 1024;
+	
 		Thread():
 			m_thread(pthread_self())
 		{
@@ -21,7 +25,10 @@ namespace dyplo
 
 		int start(void *(*start_routine) (void *), void *arg)
 		{
-			return pthread_create(&m_thread, NULL, start_routine, arg);
+			pthread_attr_t attr;
+			pthread_attr_init(&attr);
+			pthread_attr_setstacksize(&attr, default_stack_size);
+			return pthread_create(&m_thread, &attr, start_routine, arg);
 		}
 
 		int join(void **retval = NULL)
