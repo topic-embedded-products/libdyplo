68ac96c45b82d61897eeb6976eb75f2bfb2f7f55
Mike Looijmans <mike.looijmans@topic.nl>
2013-10-29 13:06:22 +0100

    filequeue: Set poll timeout to infinity
    According to 'man poll', negative values are "forever".

--- filequeue.cpp
+++ filequeue.cpp
@@ -52,7 +52,7 @@ namespace dyplo
 		fds[0].events = POLLIN | POLLRDHUP | POLLERR | POLLHUP | POLLNVAL;
 		fds[1].fd = m_internal_pipe.read_handle();
 		fds[1].events = POLLIN | POLLRDHUP | POLLERR | POLLHUP | POLLNVAL;
-		int result = poll(fds, 2, 2000);
+		int result = poll(fds, 2, -1);
 		if (result == -1)
 			throw std::runtime_error("poll() failed");
 		if (fds[1].revents)
@@ -68,7 +68,7 @@ namespace dyplo
 		fds[0].events = POLLOUT | POLLERR | POLLHUP | POLLNVAL;
 		fds[1].fd = m_internal_pipe.read_handle();
 		fds[1].events = POLLIN | POLLRDHUP | POLLERR | POLLHUP | POLLNVAL;
-		int result = poll(fds, 2, 2000);
+		int result = poll(fds, 2, -1);
 		if (result == -1)
 			throw std::runtime_error("poll() failed");
 		if (fds[1].revents)
