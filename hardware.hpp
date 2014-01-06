2e2d0f7f8f4e51bd10847f6ee10adc58c3e3637e
Mike Looijmans <mike.looijmans@topic.nl>
2014-01-06 13:59:29 +0100

    License as LGPLv3 and add header to all files
    python ../copyright-header.py --license LGPL3 --software libdyplo --year 2013,2014 --description 'Dyplo library for Kahn processing networks' --holder 'Topic Embedded Products B.V. <Mike Looijmans> (http://www.topic.nl)' *.cpp *.hpp

--- hardware.hpp
+++ hardware.hpp
@@ -1,3 +1,31 @@
+/*
+ * hardware.hpp
+ *
+ * Dyplo library for Kahn processing networks.
+ *
+ * (C) Copyright 2013,2014 Topic Embedded Products B.V. <Mike Looijmans> (http://www.topic.nl).
+ * All rights reserved.
+ *
+ * This file is part of libdyplo.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA or see <http://www.gnu.org/licenses/>.
+ *
+ * You can contact Topic by electronic mail via info@topic.nl or via
+ * paper mail at the following address: Postbus 440, 5680 AK Best, The Netherlands.
+ */
 #pragma once
 
 #include <string>
