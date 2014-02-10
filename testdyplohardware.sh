add0ddeb3a81ca43b44491ee7e0e2d8dd0e2e8ab
Niek van Agt <niek.van.agt@topic.nl>
2014-02-10 15:41:17 +0100

    changed search pattern for partials to partial.* instead of partial.bit

--- testdyplohardware.sh
+++ testdyplohardware.sh
@@ -2,9 +2,9 @@
 /sbin/modprobe -r dyplo || true
 set -e
 cat /usr/share/fpga.bin > /dev/xdevcfg
-cat /usr/share/bitstreams/adder/adder_node_1_partial.bit > /dev/xdevcfg
-cat /usr/share/bitstreams/adder/adder_node_2_partial.bit > /dev/xdevcfg
-cat /usr/share/bitstreams/adder/adder_node_3_partial.bit > /dev/xdevcfg
-cat /usr/share/bitstreams/adder/adder_node_4_partial.bit > /dev/xdevcfg
+cat /usr/share/bitstreams/adder/adder_node_1_partial.* > /dev/xdevcfg
+cat /usr/share/bitstreams/adder/adder_node_2_partial.* > /dev/xdevcfg
+cat /usr/share/bitstreams/adder/adder_node_3_partial.* > /dev/xdevcfg
+cat /usr/share/bitstreams/adder/adder_node_4_partial.* > /dev/xdevcfg
 /sbin/modprobe dyplo
 /usr/bin/testdyplodriver $*
