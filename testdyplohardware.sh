2e36d6330745cb747b2e54418215c8d7015275d4
Mike Looijmans <mike.looijmans@topic.nl>
2014-09-08 09:20:39 +0200

    testdyplohardware.sh: Set programming mode to "full" first
    Set the xdevcfg into "full" mode before replacing the bitstream. This
    makes the driver reset the FPGA after programming.

--- testdyplohardware.sh
+++ testdyplohardware.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 /sbin/modprobe -r dyplo || true
 set -e
+echo 0 > /sys/class/xdevcfg/xdevcfg/device/is_partial_bitstream
 cat /usr/share/fpga.bin > /dev/xdevcfg
 /sbin/modprobe dyplo
 /usr/bin/testdyplodriver $*
